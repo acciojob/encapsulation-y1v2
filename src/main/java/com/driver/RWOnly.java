0e7c74a86c057e411c75fe37272dc4f19ed39f75
y1v2 <yash8604874858@gmail.com>
2023-05-12 18:35:39 +0530

    done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
+    private String Name;
     
+    public void setName(String n){
+        this.Name= n;
+    }
+    public void getName(){
+        System.out.println(this.Name);
+    }
 }
