61420f7beac1862fcaa9444c5ce149593d8e6bc1
y1v2 <yash8604874858@gmail.com>
2023-06-13 04:45:37 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+//        rwOnly.name = "yash";  //no access due to access modifiers
+//        System.out.println(rwOnly.name);//no access due to access modifiers
+        rwOnly.setName("yash");
+        String name = rwOnly.getName();
+        System.out.println(name);
+    }
 }
